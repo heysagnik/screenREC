fe097d75d4cef86e63d21105fef4c94073a99452
Sagnik Sahoo <sahoosagnik1@gmail.com>
2022-10-28 16:31:33 +0530

    Update logo.vue

--- src/components/logo.vue
+++ src/components/logo.vue
@@ -18,7 +18,7 @@
       <source srcset="/images/logo.png?width=400" type="image/png" />
       <img src="/images/logo.png?width=400" alt="test image" />
     </picture>
-    <h1 class="logo--text">Screen <span>REC</span></h1><sup style="font-weight:400; font-size:20">βeta</sup>
+    <h1 class="logo--text">Screen <span>REC</span> <sup>βeta</sup></h1>
   </a>
 </template>
 
@@ -71,6 +71,10 @@
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
     }
+    sup {
+      font-size: 20px;
+      vertical-align: top;
+   }
   }
 }
 </style>
